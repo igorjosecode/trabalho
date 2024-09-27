102baeae647b915ff1636762d20225a5d52b9eff
igorjosecode <RES0091174@aluno.firjansenaisesi.com.br>
2024-09-26 21:15:14 -0300

    Add files via upload

---
+++ areacirculo.js
@@ -0,0 +1,5 @@
+var n = 3.14159
+var r = prompt("insira o raio do circulo: ")
+
+var area = n * r * r
+document.write(area)
---
+++ extremamentebasico.js
@@ -0,0 +1,24 @@
+/*Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo.
+Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".*/
+
+var a = 10
+var b = 9
+var x 
+
+x = a + b 
+console.log("X = ", x)
+
+
+var a = -10
+var b = 4
+var x 
+
+x = a + b 
+console.log("X = ", x)
+
+var a = 15
+var b = -7
+var x 
+
+x = a + b 
+console.log("X = ", x)
